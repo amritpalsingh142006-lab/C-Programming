d972d161b7d904603b274760b02e89cf798217ad
amritpalsingh142006-lab <amritpalsingh142006@gmail.com>
2026-02-11 22:26:06 +0530

    Add files via upload

---
+++ product of 2 numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int num1,num2,product;
+    printf("Enter 2 Numbers");
+    scanf("%d%d",&num1,&num2);
+    product=num1*num2;
+    printf("Product=%d",product);
+}
