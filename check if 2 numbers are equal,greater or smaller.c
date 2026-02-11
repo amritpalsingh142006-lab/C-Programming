4dd5f0cf752dd23c1f1959c5cef3fecd8df4c60a
amritpalsingh142006-lab <amritpalsingh142006@gmail.com>
2026-02-11 23:01:27 +0530

    Add files via upload

---
+++ check if 2 numbers are equal,greater or smaller.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("Enter a 2 numbers");
+    scanf("%d%d",&a,&b);
+    if(a==b)
+    printf("%d is equal to %d",a,b);
+    else if(a>b)
+    printf("%d is greater than %d",a,b);
+    else if(a<b)
+    printf("%d is smaller than %d",a,b);
+    return 0;
+}
